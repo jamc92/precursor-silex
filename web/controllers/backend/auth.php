2b5daacff32518efc4a99cc22ff7abd97a57f0fc
Ramón Serrano <ramon.calle.88@gmail.com>
2014-07-03 14:47:53 -0430

    Autenticacion de usuario.

--- web/controllers/backend/auth.php
+++ web/controllers/backend/auth.php
@@ -14,14 +14,13 @@
 require_once __DIR__.'/../../../vendor/autoload.php';
 require_once __DIR__.'/../../../src/app.php';
 
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints as Assert,
+    Symfony\Component\HttpFoundation\Request;
 
-$app->get('/login', function () use ($app) {
-    return $app['twig']->render('backend/auth/login.html.twig', array());
+$app->get('/login', function (Request $request) use ($app) {
+    return $app['twig']->render('backend/auth/login.html.twig', array(
+    		'error'         => $app['security.last_error']($request),
+        	'last_username' => $app['session']->get('_security.last_username'),
+    	));
 })
 ->bind('login');
-
-$app->match('/logout', function () use ($app) {
-
-})
-->bind('logout');
