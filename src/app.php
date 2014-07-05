35c3db6f8cab610deba01e76beb862dc42ab1624
Ramón Serrano <ramon.calle.88@gmail.com>
2014-07-05 10:33:09 -0430

    Control de acceso a url con autenticación de usuarios.

--- src/app.php
+++ src/app.php
@@ -26,17 +26,33 @@ $app->register(new Silex\Provider\TranslationServiceProvider(), array(
 $app->register(new Silex\Provider\ValidatorServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
-$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+app->register(new Silex\Provider\SecurityServiceProvider(), array(
         'security.firewalls' => array(
-            'admin' => array(
-                'pattern' => '^/admin',
-                'form' => array('login_path' => '/login', 'check_path' => '/admin/login_check'),
-                'logout' => array('logout_path' => '/admin/logout'),
-                'users' => $app->share(function () use ($app) {
-                    return new UserProvider($app['db']);
-                }),
+            'login_path' => array(
+                'pattern' => '^/login$',
+                'anonymous' => true
             ),
+            'default' => array(
+                'pattern' => '^/.*$',
+                'anonymous' => true,
+                'form' => array(
+                    'login_path' => '/login',
+                    'check_path' => '/login_check',
+                ),
+                'logout' => array(
+                    'logout_path' => '/logout',
+                    'invalidate_session' => false
+                ),
+                'users' => $app->share(function($app) { 
+                    return new UserProvider($app['db']); 
+                }),
+            )
         ),
+        'security.access_rules' => array(
+            array('^/login$', 'IS_AUTHENTICATED_ANONYMOUSLY'),
+            array('^/.+$', 'ROLE_ADMIN'),
+            array('^/admin', 'ROLE_USER')
+        )
     ));
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
 
