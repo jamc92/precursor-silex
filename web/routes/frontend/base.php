68f6cd6585b7684c0f5bae952df40c6ba89eb61d
Administrador <ramon.calle.88@gmail.com>
2014-07-13 23:33:23 -0430

    Mostrar las categorias de los articulos.

--- web/routes/frontend/base.php
+++ web/routes/frontend/base.php
@@ -2,7 +2,28 @@
 
 $app->match('/', function () use ($app) {
 
-    return $app['twig']->render('frontend/index.html.twig', array());
+    $categorias = array();
+
+    $categorias_sql = "SELECT * FROM `categoria` WHERE id > 1";
+    $categoria_sql  = $app['db']->fetchAll($categorias_sql, array());
+
+    foreach ($categoria_sql as $cat_key => $cat_value) {
+        $categorias[$cat_key] = $cat_value;
+    }
+
+    $articulos = array();
+
+    $articulos_sql = "SELECT * FROM `articulo` WHERE id > 1";
+    $articulo_sql  = $app['db']->fetchAll($articulos_sql, array());
+
+    foreach ($articulo_sql as $art_key => $art_value) {
+        $articulos[$art_key] = $art_value;
+    }
+
+    return $app['twig']->render('frontend/index.html.twig', array(
+        'categorias' => $categorias,
+        'articulos' => $articulos
+    ));
         
 })
 ->bind('home');
