53c3754684f54ed42278c89eb157df3cff24a4e1
Administrador <ramon.calle.88@gmail.com>
2014-07-14 19:56:23 -0430

    Actualizado el UploadImage.

--- vendor/precursor/precursor/src/Precursor/UploadImage.php
+++ vendor/precursor/precursor/src/Precursor/UploadImage.php
@@ -10,4 +10,96 @@ namespace Precursor;
 
 class UploadImage {
 
+    /**
+     * @var array
+     */
+    var $allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
+
+    /**
+     * @var bool
+     */
+    var $ignore_uploads = false;
+
+    /**
+     * @var string
+     */
+    var $upload_dir = '';
+
+    /**
+     * @param $status
+     * @param $vars
+     * @param $type
+     * @return JSON|string
+     */
+    protected function exitStatus($status, $vars = array(), $type = 'json') {
+        if (strtolower($type) == 'json') {
+            return json_encode(
+                array(
+                    'status' => $status,
+                    'vars' => $vars
+                )
+            );
+        } else {
+            return array(
+                'status' => $status,
+                'vars' => $vars
+            );
+        }
+    }
+
+    /**
+     * @param $file_name
+     * @return string
+     */
+    protected function getExtension($file_name) {
+        $ext = explode('.', $file_name);
+        $ext = array_pop($ext);
+        return strtolower($ext);
+    }
+
+    /**
+     * @param $file
+     * @return JSON
+     */
+    public function uploadFile($file) {
+
+        if ($file['error'] == 0) {
+
+            if (!in_array( $this->getExtension($file['name']), $this->allowed_ext)) {
+                return $this->exitStatus('Only '.implode(',',$this->allowed_ext).' files are allowed!');
+            }
+
+            if ($this->ignore_uploads) {
+                // File uploads are ignored. We only log them.
+
+                $line = implode('		', array( date('r'), $_SERVER['REMOTE_ADDR'], $file['size'], $file['name']));
+                file_put_contents('log.txt', $line.PHP_EOL, FILE_APPEND);
+
+                return $this->exitStatus('Uploads are ignored in demo mode.');
+            }
+
+            // Create the upload dir if don't exists
+            if (!is_null($this->upload_dir) && !file_exists($this->upload_dir)) {
+                @mkdir($this->upload_dir);
+            }
+
+            // Date
+            $date = date("d-m-Y");
+            if (!file_exists($this->upload_dir . "$date")) {
+                @mkdir($this->upload_dir . "$date");
+            }
+
+            // Move the uploaded file from the temporary
+            // directory to the uploads folder:
+
+            if(move_uploaded_file($file['tmp_name'], $this->upload_dir . "$date/" . urlencode($file['name']))) {
+                $message = array(
+                    'imagen' => urlencode($file['name']),
+                );
+                return $this->exitStatus('File was uploaded successfuly!', $message, 'array');
+            }
+        } else {
+            return $this->exitStatus('Something went wrong with your upload!');
+        }
+    }
 } 
