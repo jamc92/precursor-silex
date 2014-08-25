f86169c1b51e1e0b688bdfc50ccb8b944359fa6a
Administrador <ramon.calle.88@gmail.com>
2014-08-25 00:12:10 -0430

    Modificado el callback de las rutas.

--- web/routes/backend/archivo.php
+++ web/routes/backend/archivo.php
@@ -1,26 +1,26 @@
 <?php
 
-$app->match('/admin/archivo', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::ver')
+$app->match('/admin/archivo', 'Precursor\\Application\\Controller\\Backend\\Archivo::ver')
     ->bind('archivo_list');
 
-$app->match('/admin/archivo/syntax', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::sintaxis')
+$app->match('/admin/archivo/syntax', 'Precursor\\Application\\Controller\\Backend\\Archivo::sintaxis')
     ->bind('archivo_syntax');
 
-$app->match('/admin/archivo/create', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::agregar')
+$app->match('/admin/archivo/create', 'Precursor\\Application\\Controller\\Backend\\Archivo::agregar')
     ->bind('archivo_create')
     ->method("GET|POST|PUT");
 
-$app->match('/admin/archivo/edit/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::editar')
+$app->match('/admin/archivo/edit/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Archivo::editar')
     ->bind('archivo_edit');
 
-$app->match('/admin/archivo/delete/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::eliminar')
+$app->match('/admin/archivo/delete/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Archivo::eliminar')
     ->bind('archivo_delete');
 
-$app->match('/admin/archivo/protected', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::ver')
+$app->match('/admin/archivo/protected', 'Precursor\\Application\\Controller\\Backend\\Archivo::ver')
     ->bind('archivo_protected_list');
 
-$app->match('/admin/archivo/protected/edit/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::editar')
+$app->match('/admin/archivo/protected/edit/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Archivo::editar')
     ->bind('archivo_protected_edit');
 
-$app->match('/admin/archivo/protected/delete/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Backend\\Archivo::eliminar')
+$app->match('/admin/archivo/protected/delete/{nombre}', 'Precursor\\Application\\Controller\\Backend\\Archivo::eliminar')
     ->bind('archivo_protected_delete');
