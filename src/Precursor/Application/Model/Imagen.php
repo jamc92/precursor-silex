270f3ca42d7fa4252d9d401b17cc1fc9b6bb2ab0
Administrador <ramon.calle.88@gmail.com>
2014-09-11 20:11:37 -0430

    Agregado funciones

--- src/Precursor/Application/Model/Imagen.php
+++ src/Precursor/Application/Model/Imagen.php
@@ -13,10 +13,77 @@ use Doctrine\DBAL\Connection,
 
 class Imagen extends Model
 {
-	
-	function __construct(Connection $db)
-	{
-		parent::__construct($db, 'imagen');
-	}
 
+    /**
+     * @param Connection $db Objeto de la conección de doctrine con la base de datos
+     */
+    function __construct(Connection $db)
+    {
+        parent::__construct($db, 'imagen');
+    }
+    
+    /**
+     * @param array $fields Campos que se desean obtener
+     * @return array Arreglo de las imagenes
+     */
+    public function getImagenes(array $fields = array())
+    {
+        if (empty($fields)) {
+            $fields = array(
+                'id', 'nombre', 'link'
+            );
+        }
+        return $this->_selectFields($fields);
+    }
+
+    /**
+     * @param string $nombre Nombre de la imagen
+     * @param string $link   URL de la imagen
+     * @param string $imagen Puede ser la imagen misma en tipo MIME
+     * @return int Filas afectadas
+     */
+    public function guardar($nombre, $link, $imagen = '')
+    {
+        $data = array(
+            'nombre' => $nombre,
+            'link'   => $link,
+            'creado' => date('Y-m-d H:m:s')
+        );
+        
+        if ($imagen != '' && !is_null($imagen)) {
+            $data['imagen'] = $imagen;
+        }
+        
+        return $this->_insert($data);
+    }
+    
+    /**
+     * @param int $id        Id de la imagen
+     * @param string $nombre Nombre de la imagen
+     * @param string $link   URL de la imagen
+     * @param string $imagen Puede se la imagen misma en tipo MIME
+     * @return int Filas afectadas
+     */
+    public function modificar($id, $nombre, $link, $imagen = '')
+    {
+        $data = array(
+            'nombre' => $nombre,
+            'link'   => $link
+        );
+        
+        if ($imagen != '' && !is_null($imagen)) {
+            $data['imagen'] = $imagen;
+        }
+        
+        return $this->_update($data, array('id' => $id));
+    }
+    
+    /**
+     * @param int $id Id de la imagen
+     * @return int Filas afectadas
+     */
+    public function eliminar($id)
+    {
+        return $this->_delete(array('id' => $id));
+    }
 } 
