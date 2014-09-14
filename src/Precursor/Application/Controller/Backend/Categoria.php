48a3ff4eabf7bee9683026136bb7a6ce09195733
Administrador <ramon.calle.88@gmail.com>
2014-09-14 19:17:41 -0430

    Modelo de categoria agregado funciones. Interaccion entre el modelo y el controlador.

--- src/Precursor/Application/Controller/Backend/Categoria.php
+++ src/Precursor/Application/Controller/Backend/Categoria.php
@@ -22,33 +22,11 @@ class Categoria {
      */
     public function ver(Request $request, Application $app)
     {
-        $table_columns = array(
-            'id',
-            'categoria',
-            'nombre',
-            'creado',
-            'modificado',
-        );
-
-        $primary_key = "id";
-        $rows = array();
-
-        $find_sql = "SELECT `categoria`.*, c2.nombre as categoria FROM `categoria` INNER JOIN `categoria` c2 ON `categoria`.id_categoria = c2.id";
-        $rows_sql = $app['db']->fetchAll($find_sql, array());
-
-        foreach($rows_sql as $row_key => $row_sql){
-            for($i = 0; $i < count($table_columns); $i++){
-
-                $rows[$row_key][$table_columns[$i]] = $row_sql[$table_columns[$i]];
-
-
-            }
-        }
+        $categoriaModelo = new \Precursor\Application\Model\Categoria($app['db']);
+        $categorias = $categoriaModelo->getCategorias();
 
         return $app['twig']->render('backend/categoria/list.html.twig', array(
-            "table_columns" => $table_columns,
-            "primary_key" => $primary_key,
-            "rows" => $rows
+            "categorias" => $categorias
         ));
     }
 
@@ -59,12 +37,13 @@ class Categoria {
      */
     public function agregar(Request $request, Application $app)
     {
-        $find_sql = "SELECT * FROM `categoria`";
-        $rows_sql = $app['db']->fetchAll($find_sql, array());
+        $categoriaModelo = new \Precursor\Application\Model\Categoria($app['db']);
+        $categorias = $categoriaModelo->getTodo();
+        
         $options = array();
 
-        foreach($rows_sql as $row_key => $row_sql) {
-            $options[$row_sql['id']] = $row_sql['nombre'];
+        foreach($categorias as $categoria) {
+            $options[$categoria['id']] = $categoria['nombre'];
         }
 
         $initial_data = array(
@@ -76,7 +55,7 @@ class Categoria {
 
         $form = $form->add('categoria_superior', 'choice', array(
             'choices' => $options,
-            'required' => false
+            'required' => true
         ));
 
         $form = $form->add('nombre', 'text', array('required' => true));
@@ -90,18 +69,18 @@ class Categoria {
             if ($form->isValid()) {
                 $data = $form->getData();
 
-                $update_query = "INSERT INTO `categoria` (`id_categoria`, `nombre`, `creado`, `modificado`) VALUES (?, ?, NOW(), NOW())";
-                $app['db']->executeUpdate($update_query, array($data['categoria_superior'], $data['nombre']));
+                $filasAfectadas = $categoriaModelo->guardar($data['categoria_superior'], $data['nombre']);
 
-
-                $app['session']->getFlashBag()->add(
-                    'success',
-                    array(
-                        'message' => '¡Categoría creada!',
-                    )
-                );
+                if ($filasAfectadas == 1) {
+                    $app['session']->getFlashBag()->add(
+                        'success',
+                        array(
+                            'message' => '¡Categoría creada!',
+                        )
+                    );
+                }
+                
                 return $app->redirect($app['url_generator']->generate('categoria_list'));
-
             }
         }
 
@@ -118,10 +97,61 @@ class Categoria {
      */
     public function editar(Request $request, Application $app, $id)
     {
-        $find_sql = "SELECT * FROM `categoria` WHERE `id` = ?";
-        $row_sql = $app['db']->fetchAssoc($find_sql, array($id));
+        $categoriaModelo = new \Precursor\Application\Model\Categoria($app['db']);
+        
+        $categoria = $categoriaModelo->getPorId($id);
+        
+        if (!empty($categoria)) {
+            $categorias = $categoriaModelo->getTodo();
 
-        if(!$row_sql){
+            $options = array();
+
+            foreach($categorias as $categoria) {
+                $options[$categoria['id']] = $categoria['nombre'];
+            }
+
+            $initial_data = array(
+                'nombre' => $categoria['nombre'],
+            );
+
+            $form = $app['form.factory']->createBuilder('form', $initial_data);
+
+            $form = $form->add('categoria_superior', 'choice', array(
+                'choices' => $options,
+                'data' => $categoria['id_categoria'],
+                'required' => false
+            ));
+            $form = $form->add('nombre', 'text', array('required' => true));
+
+            $form = $form->getForm();
+
+            if("POST" == $request->getMethod()){
+
+                $form->handleRequest($request);
+
+                if ($form->isValid()) {
+                    $data = $form->getData();
+
+                    $filasAfectadas = $categoriaModelo->modificar($id, $data['categoria_superior'], $data['nombre']);
+                    
+                    if ($filasAfectadas == 1) {
+                        $app['session']->getFlashBag()->add(
+                            'info',
+                            array(
+                                'message' => 'Categoría editada!',
+                            )
+                        );
+                    }
+                    
+                    return $app->redirect($app['url_generator']->generate('categoria_edit', array("id" => $id)));
+                }
+            }
+
+            return $app['twig']->render('backend/categoria/edit.html.twig', array(
+                "form" => $form->createView(),
+                "id" => $id
+            ));
+        } else {
             $app['session']->getFlashBag()->add(
                 'warning',
                 array(
@@ -130,57 +160,6 @@ class Categoria {
             );
             return $app->redirect($app['url_generator']->generate('categoria_list'));
         }
-
-        $find_sql = "SELECT * FROM `categoria`";
-        $rows_sql = $app['db']->fetchAll($find_sql, array());
-
-        foreach($rows_sql as $row_key => $row_sql2) {
-            if ($row_sql['id_categoria'] != $row_sql2['id_categoria']) {
-                $options[$row_sql2['id']] = $row_sql2['nombre'];
-            }
-        }
-
-        $initial_data = array(
-            'nombre' => $row_sql['nombre'],
-        );
-
-        $form = $app['form.factory']->createBuilder('form', $initial_data);
-
-        $form = $form->add('categoria_superior', 'choice', array(
-            'choices' => $options,
-            'data' => $row_sql['id_categoria'],
-            'required' => false
-        ));
-        $form = $form->add('nombre', 'text', array('required' => true));
-
-        $form = $form->getForm();
-
-        if("POST" == $request->getMethod()){
-
-            $form->handleRequest($request);
-
-            if ($form->isValid()) {
-                $data = $form->getData();
-
-                $update_query = "UPDATE `categoria` SET `id_categoria` = ?, `nombre` = ? WHERE `id` = ?";
-                $app['db']->executeUpdate($update_query, array($data['categoria_superior'], $data['nombre'], $id));
-
-
-                $app['session']->getFlashBag()->add(
-                    'info',
-                    array(
-                        'message' => 'Categoría editada!',
-                    )
-                );
-                return $app->redirect($app['url_generator']->generate('categoria_edit', array("id" => $id)));
-
-            }
-        }
-
-        return $app['twig']->render('backend/categoria/edit.html.twig', array(
-            "form" => $form->createView(),
-            "id" => $id
-        ));
     }
 
     /**
@@ -191,21 +170,22 @@ class Categoria {
      */
     public function eliminar(Request $request, Application $app, $id)
     {
-        $find_sql = "SELECT * FROM `categoria` WHERE `id` = ?";
-        $row_sql = $app['db']->fetchAssoc($find_sql, array($id));
-
-        if($row_sql){
-            $delete_query = "DELETE FROM `categoria` WHERE `id` = ?";
-            $app['db']->executeUpdate($delete_query, array($id));
-
-            $app['session']->getFlashBag()->add(
-                'info',
-                array(
-                    'message' => '¡Categoría eliminada!',
-                )
-            );
-        }
-        else{
+        $categoriaModelo = new \Precursor\Application\Model\Categoria($app['db']);
+        
+        $categoria = $categoriaModelo->getPorId($id);
+        
+        if (!empty($categoria)) {
+            $filasAfectadas = $categoriaModelo->eliminar($id);
+            
+            if ($filasAfectadas == 1) {
+                $app['session']->getFlashBag()->add(
+                    'info',
+                    array(
+                        'message' => '¡Categoría eliminada!',
+                    )
+                );
+            }
+        } else {
             $app['session']->getFlashBag()->add(
                 'warning',
                 array(
--- src/Precursor/Application/Model/Categoria.php
+++ src/Precursor/Application/Model/Categoria.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Description of Categoria.php.
  * 
@@ -11,12 +12,65 @@ namespace Precursor\Application\Model;
 use Doctrine\DBAL\Connection,
     Precursor\Application\Model;
 
-class Categoria extends Model
-{
-	
-	function __construct(Connection $db)
-	{
-		parent::__construct($db, 'categoria');
-	}
-	
-} 
+class Categoria extends Model {
+
+    /**
+     * @param Connection $db Objeto de la conección de doctrine con la base de datos
+     */
+    function __construct(Connection $db)
+    {
+        parent::__construct($db, 'categoria');
+    }
+    
+    public function getCategorias(array $fields = array())
+    {
+        if (empty($fields)) {
+            $fields = array(
+                'categoria.*',
+                'c.nombre as categoria'
+            );
+        }
+        $join = array('categoria c', 'categoria.id_categoria', 'c.id', '=');
+        return $this->getTodo($fields, $join);
+    }
+    
+    /**
+     * @param int $id_categoria Id de la categoría
+     * @param string $nombre    Nombre de la categoría
+     * @return int Filas afectadas
+     */
+    public function guardar($id_categoria = 1, $nombre)
+    {
+        $data = array(
+            'id_categoria' => $id_categoria,
+            'nombre'       => $nombre,
+            'creado'       => date('Y:m:d H:m:s')
+        );
+        return $this->_insert($data);
+    }
+
+    /**
+     * 
+     * @param int $id           Id de la categoría
+     * @param int $id_categoria Id de la categoría superior
+     * @param string $nombre    Nombre de la categoría
+     * @return int Filas afectadas
+     */
+    public function modificar($id, $id_categoria = 1, $nombre)
+    {
+        $data = array(
+            'id_categoria' => $id_categoria,
+            'nombre'       => $nombre
+        );
+        return $this->_update($data, array('id' => $id));
+    }
+    
+    /**
+     * @param int $id Id de la categoría
+     * @return int Filas afectadas
+     */
+    public function eliminar($id)
+    {
+        return $this->_delete(array('id' => $id));
+    }
+}
