8784786156da222e06fc3ba241bdbca7b90525c2
Administrador <ramon.calle.88@gmail.com>
2014-09-14 19:28:18 -0430

    Clase de Opcion.

--- web/routes/backend/opcion.php
+++ web/routes/backend/opcion.php
@@ -1,15 +1,15 @@
 <?php
 
-$app->match('/admin/opcion', 'Precursor\Application\Controller\Opcion::ver')
+$app->match('/admin/opcion', 'Precursor\Application\Controller\Backend\Opcion::ver')
     ->bind('opcion_list');
 
-$app->match('/admin/opcion/create', 'Precursor\Application\Controller\Opcion::agregar')
+$app->match('/admin/opcion/create', 'Precursor\Application\Controller\Backend\Opcion::agregar')
     ->bind('opcion_create');
 
-$app->match('/admin/opcion/edit/{id}', 'Precursor\Application\Controller\Opcion::editar')
+$app->match('/admin/opcion/edit/{id}', 'Precursor\Application\Controller\Backend\Opcion::editar')
     ->assert('id', '\d+')
     ->bind('opcion_edit');
 
-$app->match('/admin/opcion/delete/{id}', 'Precursor\Application\Controller\Opcion::eliminar')
+$app->match('/admin/opcion/delete/{id}', 'Precursor\Application\Controller\Backend\Opcion::eliminar')
     ->assert('id', '\d+')
     ->bind('opcion_delete');
