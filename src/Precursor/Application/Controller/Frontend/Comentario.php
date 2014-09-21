0815cb212155ec47d2ccf877d7a1d8697a4c8ffb
Administrador <ramon.calle.88@gmail.com>
2014-09-21 17:42:38 -0430

    Controlador de Comentario.

---
+++ src/Precursor/Application/Controller/Frontend/Comentario.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Description of Comentario
+ *
+ * @author Ramón Serrano <ramon.calle.88@gmail.com>
+ * @subpackage Frontend
+ */
+
+namespace Precursor\Application\Controller\Frontend;
+
+class Comentario
+{
+
+    public function getComentariosArticulo($idArticulo) {
+        
+    }
+
+}
