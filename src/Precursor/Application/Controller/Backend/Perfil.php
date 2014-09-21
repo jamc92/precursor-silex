f1900d068c88f7fa4d3fb88cbe2208c058949120
Administrador <ramon.calle.88@gmail.com>
2014-09-21 01:20:14 -0430

    Funciones del modelo agregadas. Interaccion del modelo y controlador.

--- src/Precursor/Application/Controller/Backend/Perfil.php
+++ src/Precursor/Application/Controller/Backend/Perfil.php
@@ -23,30 +23,20 @@ class Perfil
      */
     public function ver(Request $request, Application $app)
     {
-        $table_columns = array(
-            'id',
-            'nombre',
-            'creado',
-            'modificado',
-        );
-
-        $primary_key = "id";
-        $rows = array();
-
-        $find_sql = "SELECT * FROM `perfil`";
-        $rows_sql = $app['db']->fetchAll($find_sql, array());
-
-        foreach ($rows_sql as $row_key => $row_sql) {
-            for ($i = 0; $i < count($table_columns); $i++) {
-
-                $rows[$row_key][$table_columns[$i]] = $row_sql[$table_columns[$i]];
+        $perfilModelo = new \Precursor\Application\Model\Perfil($app['db']);
+        $perfiles = $perfilModelo->getTodo();
+        
+        $perfilesImportantes = $perfilModelo->getPerfilesImportantes();
+        foreach ($perfiles as $i => $perfil) {
+            if (in_array($perfil['id'], $perfilesImportantes)) {
+                $perfiles[$i]['importante'] = true;
+            } else {
+                $perfiles[$i]['importante'] = false;
             }
         }
 
         return $app['twig']->render('backend/perfil/list.html.twig', array(
-            "table_columns" => $table_columns,
-            "primary_key" => $primary_key,
-            "rows" => $rows
+            'perfiles' => $perfiles
         ));
     }
 
@@ -58,10 +48,7 @@ class Perfil
     public function agregar(Request $request, Application $app)
     {
         $initial_data = array(
-            'id' => '',
-            'nombre' => '',
-            'creado' => '',
-            'modificado' => '',
+            'nombre' => ''
         );
 
         $form = $app['form.factory']->createBuilder('form', $initial_data);
@@ -77,15 +64,24 @@ class Perfil
             if ($form->isValid()) {
                 $data = $form->getData();
 
-                $update_query = "INSERT INTO `perfil` (`id`, `nombre`, `creado`, `modificado`) VALUES (null, ?, NOW(), NOW())";
-                $app['db']->executeUpdate($update_query, array($data['nombre']));
-
-
-                $app['session']->getFlashBag()->add(
-                    'success', array(
-                        'message' => '¡Perfil creado!',
-                    )
-                );
+                $perfilModelo = new \Precursor\Application\Model\Perfil($app['db']);
+                if ($perfilModelo->existe($data['nombre'])) {
+                    $app['session']->getFlashBag()->add(
+                        'warning', array(
+                            'message' => '¡Perfil ya existe!',
+                        )
+                    );
+                } else {
+                    $filasAfectadas = $perfilModelo->guardar($data['nombre']);
+                    
+                    if ($filasAfectadas == 1) {
+                        $app['session']->getFlashBag()->add(
+                            'success', array(
+                                'message' => '¡Perfil creado!',
+                            )
+                        );
+                    }
+                }
                 return $app->redirect($app['url_generator']->generate('perfil_list'));
             }
         }
@@ -103,10 +99,61 @@ class Perfil
      */
     public function editar(Request $request, Application $app, $id)
     {
-        $find_sql = "SELECT * FROM `perfil` WHERE `id` = ?";
-        $row_sql = $app['db']->fetchAssoc($find_sql, array($id));
+        $perfilModelo = new \Precursor\Application\Model\Perfil($app['db']);
+        
+        $perfilesImportantes = $perfilModelo->getPerfilesImportantes();
+        
+        if (in_array($id, $perfilesImportantes)) {
+            $app['session']->getFlashBag()->add(
+                'warning', array(
+                    'message' => '¡Perfil no puede ser editado!',
+                )
+            );
+            return $app->redirect($app['url_generator']->generate('perfil_list'));
+        }
+        
+        $perfil = $perfilModelo->getPorId($id);
 
-        if (!$row_sql) {
+        if (!empty($perfil)) {
+            
+            $initial_data = array(
+                'nombre' => $perfil['nombre'],
+            );
+
+            $form = $app['form.factory']->createBuilder('form', $initial_data);
+
+            $form = $form->add('nombre', 'text', array('required' => true));
+
+            $form = $form->getForm();
+
+            if ("POST" == $request->getMethod()) {
+
+                $form->handleRequest($request);
+
+                if ($form->isValid()) {
+                    $data = $form->getData();
+
+                    if ($perfilModelo->existe($data['nombre'])) {
+                        $app['session']->getFlashBag()->add(
+                            'warning', array(
+                                'message' => '¡Perfil ya existe!',
+                            )
+                        );
+                    } else {
+                        $filasAfectadas = $perfilModelo->modificar($id, $data['nombre']);
+
+                        if ($filasAfectadas == 1) {
+                            $app['session']->getFlashBag()->add(
+                                'info', array(
+                                    'message' => '¡Perfil editado!',
+                                )
+                            );
+                        }
+                    }
+                    return $app->redirect($app['url_generator']->generate('perfil_edit', array("id" => $id)));
+                }
+            }
+        } else {
             $app['session']->getFlashBag()->add(
                 'warning', array(
                     'message' => '¡Perfil no encontrado!',
@@ -115,44 +162,8 @@ class Perfil
             return $app->redirect($app['url_generator']->generate('perfil_list'));
         }
 
-
-        $initial_data = array(
-            'id' => $row_sql['id'],
-            'nombre' => $row_sql['nombre'],
-            'creado' => $row_sql['creado'],
-            'modificado' => $row_sql['modificado'],
-        );
-
-
-        $form = $app['form.factory']->createBuilder('form', $initial_data);
-
-        $form = $form->add('nombre', 'text', array('required' => true));
-
-        $form = $form->getForm();
-
-        if ("POST" == $request->getMethod()) {
-
-            $form->handleRequest($request);
-
-            if ($form->isValid()) {
-                $data = $form->getData();
-
-                $update_query = "UPDATE `perfil` SET `nombre` = ? WHERE `id` = ?";
-                $app['db']->executeUpdate($update_query, array($data['nombre'], $id));
-
-
-                $app['session']->getFlashBag()->add(
-                    'info', array(
-                        'message' => '¡Perfil editado!',
-                    )
-                );
-                return $app->redirect($app['url_generator']->generate('perfil_edit', array("id" => $id)));
-            }
-        }
-
         return $app['twig']->render('backend/perfil/edit.html.twig', array(
             "form" => $form->createView(),
-            "id" => $id
         ));
     }
 
@@ -164,18 +175,28 @@ class Perfil
      */
     public function eliminar(Request $request, Application $app, $id)
     {
-        $find_sql = "SELECT * FROM `perfil` WHERE `id` = ?";
-        $row_sql = $app['db']->fetchAssoc($find_sql, array($id));
+        $perfilModelo = new \Precursor\Application\Model\Perfil($app['db']);
+        $perfil = $perfilModelo->getPorId($id);
 
-        if ($row_sql) {
-            $delete_query = "DELETE FROM `perfil` WHERE `id` = ?";
-            $app['db']->executeUpdate($delete_query, array($id));
-
-            $app['session']->getFlashBag()->add(
-                'info', array(
-                    'message' => '¡Perfil eliminado!',
-                )
-            );
+        if (!empty($perfil)) {
+            $perfilesImportantes = $perfilModelo->getPerfilesImportantes();
+            if (in_array($id, $perfilesImportantes)) {
+                $app['session']->getFlashBag()->add(
+                    'warning', array(
+                        'message' => '¡Perfil importante! No puede ser eliminado.',
+                    )
+                );
+            } else {
+                $filasAfectadas = $perfilModelo->eliminar($id);
+                
+                if ($filasAfectadas == 1) {
+                    $app['session']->getFlashBag()->add(
+                        'info', array(
+                            'message' => '¡Perfil eliminado!',
+                        )
+                    );
+                }
+            }
         } else {
             $app['session']->getFlashBag()->add(
                 'warning', array(
@@ -183,7 +204,6 @@ class Perfil
                 )
             );
         }
-
         return $app->redirect($app['url_generator']->generate('perfil_list'));
     }
     
--- src/Precursor/Application/Model/Perfil.php
+++ src/Precursor/Application/Model/Perfil.php
@@ -21,5 +21,63 @@ class Perfil extends Model
     {
         parent::__construct($db, 'perfil');
     }
+    
+    /**
+     * @return array Ids de los Perfiles importantes
+     */
+    public function getPerfilesImportantes()
+    {
+        return array(1, 2, 3);
+    }
+    
+    /**
+     * @param string $nombre Perfil
+     * @return boolean
+     */
+    public function existe($nombre)
+    {
+        $perfil = $this->_selectFields(array(), array(), 'WHERE nombre = ?', array($nombre));
+        if (isset($perfil[0]) && !empty($perfil[0])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * @param string $nombre Perfil
+     * @return int Filas afectadas
+     */
+    public function guardar($nombre)
+    {
+        $data = array(
+            'nombre' => $nombre,
+            'creado' => date('Y-m-d H:m:s')
+        );
+        return $this->_insert($data);
+    }
+    
+    /**
+     * @param int $id Id del perfil
+     * @param string $nombre Perfil
+     * @return int Filas afectadas
+     */
+    public function modificar($id, $nombre)
+    {
+        $data = array(
+            'nombre' => $nombre
+        );
+        return $this->_update($data, array('id' => $id));
+    }
+    
+    /**
+     * @param int $id Id del perfil
+     * @return int Filas afectadas
+     */
+    public function eliminar($id)
+    {
+        return $this->_delete(array('id' => $id));
+    }
 
 }
