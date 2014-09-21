fba805d7747dd21a528f9f359e49b81a2a04a6b6
Administrador <ramon.calle.88@gmail.com>
2014-09-20 22:32:12 -0430

    Eliminada la accion de editar el comentario.

--- web/routes/backend/comentario.php
+++ web/routes/backend/comentario.php
@@ -6,10 +6,6 @@ $app->match('/admin/comentario', 'Precursor\\Application\\Controller\\Backend\\C
 $app->match('/admin/comentario/create', 'Precursor\\Application\\Controller\\Backend\\Comentario::agregar')
 ->bind('comentario_create');
 
-$app->match('/admin/comentario/edit/{id}', 'Precursor\\Application\\Controller\\Backend\\Comentario::editar')
-    ->assert('id', '\d+')
-    ->bind('comentario_edit');
-
 $app->match('/admin/comentario/delete/{id}', 'Precursor\\Application\\Controller\\Backend\\Comentario::eliminar')
     ->assert('id', '\d+')
     ->bind('comentario_delete');
