414078f127e2a9a11fd6de7293d7dbfd9e9fc85e
Administrador <ramon.calle.88@gmail.com>
2014-09-21 13:52:44 -0430

    Agregada el archivo de rutas de comentarios de articulos.

--- web/routes/backend/base.php
+++ web/routes/backend/base.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/articulo.php';
 require_once __DIR__.'/articulos_etiquetas.php';
 require_once __DIR__.'/categoria.php';
 require_once __DIR__.'/comentario.php';
+require_once __DIR__.'/comentarios_articulo.php';
 require_once __DIR__.'/etiqueta.php';
 require_once __DIR__.'/etiquetas_articulo.php';
 require_once __DIR__.'/imagen.php';
