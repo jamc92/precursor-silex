0d78822aefd22712d15c49e5b05215c838bbe812
Administrador <ramon.calle.88@gmail.com>
2014-09-21 15:22:08 -0430

    Renombrado el parametro articulo_id -> idArticulo. Camelcase

--- web/routes/backend/etiquetas_articulo.php
+++ web/routes/backend/etiquetas_articulo.php
@@ -1,14 +1,14 @@
 <?php
 
-$app->match('/admin/etiquetas_articulo/{articulo_id}', 'Precursor\\Application\\Controller\\Backend\\EtiquetasArticulo::ver')
-    ->assert('articulo_id', '\d+')
+$app->match('/admin/etiquetas_articulo/{idArticulo}', 'Precursor\\Application\\Controller\\Backend\\EtiquetasArticulo::ver')
+    ->assert('idArticulo', '\d+')
     ->bind('etiquetas_articulo_list');
 
-$app->match('/admin/etiquetas_articulo/{articulo_id}/create', 'Precursor\\Application\\Controller\\Backend\\EtiquetasArticulo::agregar')
-    ->assert('articulo_id', '\d+')
+$app->match('/admin/etiquetas_articulo/{idArticulo}/create', 'Precursor\\Application\\Controller\\Backend\\EtiquetasArticulo::agregar')
+    ->assert('idArticulo', '\d+')
     ->bind('etiquetas_articulo_create');
 
-$app->match('/admin/etiquetas_articulo/{articulo_id}/delete/{id}', 'Precursor\\Application\\Controller\\Backend\\EtiquetasArticulo::eliminar')
-    ->assert('articulo_id', '\d+')
+$app->match('/admin/etiquetas_articulo/{idArticulo}/delete/{id}', 'Precursor\\Application\\Controller\\Backend\\EtiquetasArticulo::eliminar')
+    ->assert('idArticulo', '\d+')
     ->assert('id', '\d+')
     ->bind('etiquetas_articulo_delete');
