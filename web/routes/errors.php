eed235db7d5ef015a033aafb704e9324795666e3
Administrador <ramon.calle.88@gmail.com>
2014-10-18 15:49:52 -0430

    Editado pagina de error 404.

--- web/routes/errors.php
+++ web/routes/errors.php
@@ -2,10 +2,12 @@
 
 use \PDOException,
     Doctrine\DBAL\DBALException,
+    Precursor\Application\Model\Opcion\Menu,
     Symfony\Component\HttpFoundation\Response,
     Symfony\Component\Process\Exception\LogicException,
     Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException,
-    Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+    Symfony\Component\HttpKernel\Exception\NotFoundHttpException,
+    Symfony\Component\HttpFoundation\Request;
 
 $app->error(function (PDOException $PDOException, $code) {
     return new Response($PDOException->getMessage());
@@ -19,8 +21,21 @@ $app->error(function (LogicException $logicException, $code) {
     return new Response($logicException->getMessage());
 });
 
-$app->error(function (NotFoundHttpException $notFoundHttpException, $code) {
-    return new Response($notFoundHttpException->getMessage());
+$app->error(function (NotFoundHttpException $notFoundHttpException) use($app) {
+    if (!$app['debug']) {
+        return;
+    } else {
+        $menuModelo = new Menu($app['db']);
+        $menuItems = $menuModelo->getItems();
+        
+        // Respuesta en frontend
+        $uriActual = $_SERVER['REQUEST_URI'];
+        
+        return $app['twig']->render('errors/404.html.twig', array(
+            'uri'        => $uriActual,
+            'menu_items' => $menuItems
+        ));
+    }
 });
 
 $app->error(function (MethodNotAllowedHttpException $methodNotAllowedHttpException, $code) {
